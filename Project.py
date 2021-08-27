595b77ae3c75777139d883c9bf839ce82e2569d1
ozgedincer <dncerozge@gmail.com>
2021-08-27 15:32:46 +0300

    27.08.2021

--- Project.py
+++ Project.py
@@ -4,139 +4,139 @@ import time
 import pygame
 from pygame import mixer
 
-"""pygame.init()
-pygame.display.set_caption('By Rizwan.AR')
+pygame.init()
 mixer.music.load('bgm.mp3')
-mixer.music.play(-1)"""
+mixer.music.play(-1)
 
-delay = 0.10
+#pygame.display.set_icon(pygame.image.load(“gameover.png”))
 
-pencere = turtle.Screen()
-pencere.title('Snake Game')
-pencere.bgcolor('pink')
-pencere.setup(width=600, height=600)
-pencere.tracer(0)
+velocity = 0.10
 
-kafa = turtle.Turtle()
-kafa.speed(0)
-kafa.shape("circle")
-kafa.color("brown")
-kafa.penup()
-kafa.goto(0, 100)
-kafa.direction = "stop"
+window = turtle.Screen()
+window.title('Snake Game')
+window.bgcolor('pink')
+window.setup(width=600, height=600)
+window.tracer(0)
 
-yemek = turtle.Turtle()
-yemek.speed(0)
-yemek.shape("circle")
-yemek.color("black")
-yemek.penup()
-yemek.shapesize(0.80, 0.80)
-yemek.goto(0, 0)
+head = turtle.Turtle()
+head.speed(0)
+head.shape("circle")
+head.color("brown")
+head.penup()
+head.goto(0, 100)
+head.direction = "stop"
 
-kuyruklar = []
-puan = 0
+bait = turtle.Turtle()
+bait.speed(0)
+bait.shape("circle")
+bait.color("black")
+bait.penup()
+bait.shapesize(0.80, 0.80)
+bait.goto(0, 0)
 
-yaz = turtle.Turtle()
-yaz.speed(0)
-yaz.penup()
-yaz.hideturtle()
-yaz.goto(-290, 265)
-yaz.write("Score: {}".format(puan), font=("Times New Roman", 24, "normal"))
+tails = []
+point = 0
+
+board = turtle.Turtle()
+board.speed(0)
+board.penup()
+board.hideturtle()
+board.goto(-290, 265)
+board.write("Score: {}".format(point), font=("Times New Roman", 24, "normal"))
 
 def move():
-    if kafa.direction == "up":
-        y = kafa.ycor()
-        kafa.sety(y + 20)
-    if kafa.direction == "down":
-        y = kafa.ycor()
-        kafa.sety(y - 20)
-    if kafa.direction == "right":
-        x = kafa.xcor()
-        kafa.setx(x + 20)
-    if kafa.direction == "left":
-        x = kafa.xcor()
-        kafa.setx(x - 20)
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
 
 def go_up():
-    if kafa.direction != "down":
-        kafa.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 def go_down():
-    if kafa.direction != "up":
-        kafa.direction = "down"
-
+    if head.direction != "up":
+        head.direction = "down"
 def go_right():
-    if kafa.direction != "left":
-        kafa.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 def go_left():
-    if kafa.direction != "right":
-        kafa.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
-pencere.listen()
-pencere.onkey(go_up, "Up")
-pencere.onkey(go_down, "Down")
-pencere.onkey(go_right, "Right")
-pencere.onkey(go_left, "Left")
+window.listen()
+window.onkey(go_up, "Up")
+window.onkey(go_down, "Down")
+window.onkey(go_right, "Right")
+window.onkey(go_left, "Left")
 
 while True:
-    pencere.update()
+    window.update()
 
-    if kafa.xcor() > 300 or kafa.xcor() < -300 or kafa.ycor() > 300 or kafa.ycor() < -300:
+    if head.xcor() > 300 or head.xcor() < -300 or head.ycor() > 300 or head.ycor() < -300:
         time.sleep(1)
-        kafa.goto(0, 0)
-        kafa.direction = "stop"
+        head.goto(0, 0)
+        head.direction = "stop"
 
-        for kuyruk in kuyruklar:
-            kuyruk.goto(1000, 1000)
-        kuyruklar = []
+        for tail in tails:
+            tail.goto(1000, 1000)
+        tails = []
 
-        puan = 0
-        delay = 0.10
+        point = 0
+        velocity = 0.10
 
-        yaz.clear()
-        yaz.write("Score: {}".format(puan), font=("Times New Roman", 24, "normal"))
+        board.clear()
+        board.write("Score: {}".format(point), font=("Times New Roman", 24, "normal"))
 
-    if kafa.distance(yemek) < 20:
+    if head.distance(bait) < 20:
         x = random.randint(-250, 250)
         y = random.randint(-250, 250)
-        yemek.goto(x, y)
+        bait.goto(x, y)
 
-        yeni_kuyruk = turtle.Turtle()
-        yeni_kuyruk.speed(0)
-        yeni_kuyruk.shape("circle")
-        yeni_kuyruk.color("green")
-        yeni_kuyruk.penup()
-        kuyruklar.append(yeni_kuyruk)
+        new_tail = turtle.Turtle()
+        new_tail.speed(0)
+        new_tail.shape("circle")
+        new_tail.color("green")
+        new_tail.penup()
+        tails.append(new_tail)
 
-        delay = delay + 0.01
+        velocity = velocity + 0.0001
 
-        puan = puan + 5
-        yaz.clear()
-        yaz.write("Score: {}".format(puan), font=("Times New Roman", 24, "normal"))
+        point = point + 5
+        board.clear()
+        board.write("Score: {}".format(point), font=("Times New Roman", 24, "normal"))
 
 
-    for index in range(len(kuyruklar) - 1, 0, -1):
-        x = kuyruklar[index - 1].xcor()
-        y = kuyruklar[index - 1].ycor()
-        kuyruklar[index].goto(x, y)
+    for index in range(len(tails) - 1, 0, -1):
+        x = tails[index - 1].xcor()
+        y = tails[index - 1].ycor()
+        tails[index].goto(x, y)
 
-    if len(kuyruklar) > 0:
-        x = kafa.xcor()
-        y = kafa.ycor()
-        kuyruklar[0].goto(x, y)
+    if len(tails) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        tails[0].goto(x, y)
 
     move()
 
-    for segment in kuyruklar:
-        if segment.distance(kafa) < 20:
+    for segment in tails:
+        if segment.distance(head) < 20:
             time.sleep(1)
-            kafa.goto(0, 0)
-            kafa.direction = "stop"
-            for segment in kuyruklar:
+            head.goto(0, 0)
+            head.direction = "stop"
+            for segment in tails:
                 segment.goto(1000, 1000)
-            kuyruklar = []
-            puan = 0
-            yaz.clear()
-            yaz.write('Puan: {}'.format(puan), align='center', font=('Courier', 24, 'normal'))
-            hiz = 0.15
+            tails = []
+            point = 0
+            board.clear()
+            board.write("Score: {}".format(point), font=("Times New Roman", 24, "normal"))
+            velocity = 0.15
 
-    time.sleep(delay)
+    time.sleep(velocity)
